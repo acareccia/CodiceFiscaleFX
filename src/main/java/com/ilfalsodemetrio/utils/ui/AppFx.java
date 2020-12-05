bafb013b868228d085296ea4ee83e821c17de3cb
acareccia <alberto.careccia@gmail.com>
2020-12-05 17:56:52 +0100

    it locale

--- src/main/java/com/ilfalsodemetrio/utils/ui/AppFx.java
+++ src/main/java/com/ilfalsodemetrio/utils/ui/AppFx.java
@@ -6,6 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Locale;
+
 public class AppFx extends Application {
 
     public static void main(String[] args) {
@@ -14,9 +16,11 @@ public class AppFx extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Locale.setDefault(Locale.ITALIAN);
+
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
         primaryStage.setTitle("Codice Fiscale");
-        primaryStage.setScene(new Scene(root, 400, 500));
+        primaryStage.setScene(new Scene(root, 400, 400));
         primaryStage.show();
     }
 }
