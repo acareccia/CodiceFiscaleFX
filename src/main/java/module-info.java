227375d1db689b60c37d91eb6e1fbc94ca7169d2
acareccia <alberto.careccia@gmail.com>
2020-12-11 23:02:59 +0100

    revert to gradle 6.5.1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,10 @@
-//module com.ilfalsodemetrio.CodiceFiscale {
-//    requires javafx.controls;
-//    requires javafx.fxml;
-//    requires static lombok;
-//    requires org.controlsfx.controls;
-//
-//    opens com.ilfalsodemetrio.utils.ui.fx to javafx.fxml;
-//    exports com.ilfalsodemetrio.utils.ui.fx;
-//}
+open module com.ilfalsodemetrio.utils.CodiceFiscale {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires org.controlsfx.controls;
+    requires static lombok;
+
+//    opens com.ilfalsodemetrio.utils to javafx.fxml;
+//    exports com.ilfalsodemetrio.utils;
+}
+
